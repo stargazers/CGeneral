a01a894f11bb88e9d7fea3bcc4155b72c5f5b257
Aleksi Räsänen <aleksi.rasanen@runosydan.net>
2010-11-11 21:04:52 +0200

    Doxygen comments should work now.

--- CGeneral.php
+++ CGeneral.php
@@ -1,5 +1,33 @@
 <?php
 
+/* 
+CGeneral - General methods class.
+Copyright (C) 2010 Aleksi Räsänen <aleksi.rasanen@runosydan.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+	// **************************************************
+	//	CGeneral
+	/*!
+		@brief General methods class for PHP.
+
+		@author Aleksi Räsänen
+			<aleksi.rasanen@runosydan.net>
+	*/
+	// **************************************************
 	class CGeneral
 	{
 		// **************************************************
@@ -92,7 +120,7 @@
 			@brief Convert YYYY-MM-DD to finnish
 			  way, eg. Date.Month.Year 
 
-			@param $dt Datetime
+			@param $d Datetime
 
 			@return Finnish datetime format.
 		*/
